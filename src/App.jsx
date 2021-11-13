ad9fc8d357bf3355c48646e3bcacf05ccd0a000b
Xila Luna <xilaluna2@gmail.com>
2021-11-13 13:13:10 -0800

    [add] Title comp and clean up init files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Title from "./components/Title/Title"
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Title.css"
+
+const Title = () => {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+    </div>
+  )
+}
+
+export default Title
